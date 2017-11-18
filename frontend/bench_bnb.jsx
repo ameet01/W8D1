ee04da3caae4c7d109709a689a3194e25a0e73b5
ameet vadhia <ameet01@gmail.com>
2017-11-17 17:04:46 -0800

    progress

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { configureStore } from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+// we don't put the store directly on the window because
+// it can be confusing when debugging, sometimes giving you access to state
+  // when you shouldn't
+  window.getState = store.getState;
+  window.dispatch = store.dispatch; // just for testing!
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
 });
